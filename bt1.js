6b6bc4a589fb421165460148fa3eff3f6d8b4412
thinhdh <daohuuthinh010415@gmail.com>
2023-07-19 08:00:09 +0700

    bt

---
+++ bt1.js
@@ -0,0 +1,9 @@
+let vatly = +prompt("Hãy nhập điểm môn Vật Lý");
+let hoahoc = +prompt("Hãy nhập điểm môn Hóa Học");
+let sinhhoc = +prompt("Hãy nhập điểm môn Sinh Học");
+let tongdiem = +vatly + hoahoc + sinhhoc;
+let diemtrungbinh = tongdiem / 3;
+document.writeln("Tổng điểm " + tongdiem);
+document.write("Điểm trung bình " + diemtrungbinh)
+
+
---
+++ bt2.js
@@ -0,0 +1,3 @@
+let Celsius = +prompt("Hãy nhập giá trị 0C bạn muốn đổi sang độ F")
+let F = (Celsius * 1.8) + 32 ;
+document.write("ĐỘ F là:" + F);
